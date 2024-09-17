71fc4f8e6805b98af70564197776821648d1a2c4
Manishsj1809 <manishsjain1947@gmail.com>
2024-09-17 20:26:28 +0530

    2nd

---
+++ DummyVtiger2/src/test/java/products/JenkinsExceution2.java
@@ -0,0 +1,14 @@
+package products;
+
+import org.testng.annotations.Test;
+
+public class JenkinsExceution2 {
+	
+	@Test
+	
+	public void JenkinsExceution2()
+
+	{
+		System.out.println("helloo world");
+	}
+}
