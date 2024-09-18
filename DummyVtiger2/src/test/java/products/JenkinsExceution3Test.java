bc6cdaec5b5648a210fe4c30cf46a6197dee9a40
Manishsj1809 <manishsjain1947@gmail.com>
2024-09-18 20:17:00 +0530

    2

---
+++ DummyVtiger2/src/test/java/products/JenkinsExceution3Test.java
@@ -0,0 +1,13 @@
+package products;
+
+import org.testng.annotations.Test;
+
+public class JenkinsExceution3Test {
+	@Test
+	
+	public void JenkinsExceution3Test()
+	{
+		System.out.println("poll scm2");
+	}
+
+}
